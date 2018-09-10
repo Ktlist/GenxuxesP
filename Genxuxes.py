b8ad916e21692dc4ae3a1ccb0c76448242831e9d
anmurlo <anmurlo@alumni.upv.es>
2018-09-11 00:57:56 +0200

    Se ha adaptado paraq generar 2 xuxes de forma correcta y hacer que la generacion del txt de label y bounding boxes sea opcional

--- Genxuxes.py
+++ Genxuxes.py
@@ -28,6 +28,7 @@ ap.add_argument("-i1", "--image1", required=True, help="name of xuxe kind")
 ap.add_argument("-i2", "--image2", required=False, help="name of second xuxe kind, optional")
 ap.add_argument("-n", "--number", required=True, help="Number of images resulting")
 ap.add_argument("-d", "--outdir", required=False, help="output directory, default is Results")
+ap.add_argument("-m", "--meta", required=True, help="determines wether the program generates  metadata for each image or not", default= True)
 args = vars(ap.parse_args())
 
 if args["outdir"] is None:
@@ -42,7 +43,7 @@ Tags = 1
 BB2 = 1
 BB2s=1
 numeroFondos= 4
-
+res=(320,240)
 # cuenta el numero de imagenes en el directorio
 for root1, dirs1, files1 in os.walk("Xuxes/" + args["image1"]):
     for f in files1:
@@ -61,13 +62,20 @@ if args["image2"] is not None:
 for i in range(int(args["number"])):
     # for i in range(4):
     pathf = "Xuxes/fondos/" + "fondo" + str(random.randint(1, numeroFondos)) + ".jpg"
+    #carga el fondo
     fondo = Image.open(pathf)
+    #reescala el fondo al tamaño de la foto de la Raspberrypi
+    fondo.thumbnail(res)
+
+
     # cargar xuxe 1 aleatoria de las existenetes en el directorio
     path1 = "Xuxes/" + args["image1"] + "/" + args["image1"] + str(random.randint(1, n1)) + ".png"
     xuxe1 = Image.open(path1)
     xuxe1.load()
-    # xuxe1.show()
-    xuxe1.thumbnail((128, 128))
+    # reescala la xuxe a un tamaño aleatorio entre un minimo y la mitad de la resolucion del fondo,
+    #la idea es variar el tamaño de los objetos a reconocer
+    rand_res=(random.randint(0,res(1)/2),random.randint(0,res(2)/2))
+    xuxe1.thumbnail((rand_res(0), rand_res(1)))
     # xuxe1.show()
     xuxe1 = xuxe1.rotate(random.randint(0, 360))  # se rota de forma aleatoria
     BB1 = xuxe1.getbbox()
@@ -81,7 +89,8 @@ for i in range(int(args["number"])):
         path2 = "Xuxes/" + args["image2"] + "/" + args["image2"] + str(random.randint(1, n2)) + ".png"
         xuxe2 = Image.open(path2)
         # xuxe2.show()
-        xuxe2.thumbnail((128, 128))
+        rand_res = (random.randint(0, res(1) / 2), random.randint(0, res(2) / 2))
+        xuxe2.thumbnail((rand_res(0), rand_res(1)))
         # xuxe2.show()
         xuxe2 = xuxe2.rotate(random.randint(0, 360))  # se rota de forma aleatoria
         BB2 = xuxe2.getbbox()
@@ -93,24 +102,25 @@ for i in range(int(args["number"])):
                 random.randint(0 - BB1[0], (fondo.height - BB1[3])))
     fondo.paste(xuxe1, position, xuxe1)
     b = (float(BB1[0])+position[0], float(BB1[2])+position[0], float(BB1[1])+ position[1], float(BB1[3])+ position[1])
-    BB1s =convert((141,141),b)
+    BB1s =convert(res,b)
     # se repite para segunda xuxe si necesario
     if args["image2"] is not None:
         position = (random.randint(0 - BB2[0], (fondo.width - (BB2[2]))),
                     random.randint(0 - BB2[1], (fondo.height - (BB2[3]))))
         fondo.paste(xuxe2, position, xuxe2)
-        BB2s = [position[0] + BB2[0], position[1] + BB2[1], BB2[2] + position[0], BB2[3] + position[1]]
+        b = (float(BB2[0]) + position[0], float(BB2[2]) + position[0], float(BB2[1]) + position[1],float(BB2[3]) + position[1])
+        BB2s = convert(res, b)
 
-    #test2=ImageDraw.Draw(fondo)
-    #test2.rectangle(BB1,outline='red')
-    #fondo.show()
+    #una vez pegadas las partes se guarda el resultado con el nombre de la xuxe generada + un numero
     fondo.save(outdir + str(args["image1"]) + str(i) + ".jpg")
     fondo.close()
-    f = open(outdir + str(args["image1"]) + str(i) + ".txt", 'w')
-    if args["image2"] is not None:
-        f.write(str(Tags) + ";" + str(args["image1"]) + ";" + BB1s + ";" + str(args["image2"]) + ";" + str(BB2s))
-    else:
-        f.write(str(LabelSW.get(args["image1"])) + " " + str(BB1s[0])+" " +str(BB1s[1])+" " +str(BB1s[2])+" " +str(BB1s[3]))
-    f.close()
+
+    if args["meta"] is True:
+        f = open(outdir + str(args["image1"]) + str(i) + ".txt", 'w')
+        if args["image2"] is not None:
+            f.write(str(Tags) + ";" + str(args["image1"]) + ";" + BB1s + ";" + str(args["image2"]) + ";" + str(BB2s))
+        else:
+            f.write(str(LabelSW.get(args["image1"])) + " " + str(BB1s[0])+" " +str(BB1s[1])+" " +str(BB1s[2])+" " +str(BB1s[3]))
+        f.close()
     # cv2.waitKey()
 # print(fondo)
